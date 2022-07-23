ffa27b759a222df00ba0df3cde24e85664cabb3f
kanishka2901 <kanishkaraj01@gmail.com>
2022-07-23 15:54:59 +0530

    Download api called

--- src/pages/music/Music.js
+++ src/pages/music/Music.js
@@ -9,8 +9,20 @@ import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
 import AddRoundedIcon from '@mui/icons-material/AddRounded';
 import MoreHorizTwoToneIcon from '@mui/icons-material/MoreHorizTwoTone';
 import FileDownloadRoundedIcon from '@mui/icons-material/FileDownloadRounded';
-
+import Axios from 'axios';
+import base_url from '../../components/API/Bootapi';
+import FileDownload from "js-file-download";
 const Music = ({song}) => {
+  const download =(e) =>{
+    e.preventDefault()
+    Axios({
+      url:`${base_url}`,
+      method:"GET",
+      responseType:"blob"
+    }).then((res)=>{
+      FileDownload(res.data)
+    })
+  }
     return (
         <div className='song-list'>
             {
@@ -23,7 +35,7 @@ const Music = ({song}) => {
                     <div>
                       <DeleteIcon className='song-card-icons'  />
                       <EditOutlinedIcon className='song-card-icons'/>                    
-                      <FileDownloadRoundedIcon className='song-card-icons'/>
+                      <FileDownloadRoundedIcon onClick={(e)=>download(e)} className='song-card-icons'/>
                       <MoreHorizTwoToneIcon className='song-card-icons'/>
                     </div>
                 </div>
